f1c654e95ae0688de3706637717105c7a3fd2dd3
Daniel <d.martin@web-service-kaupa.de>
2024-02-15 11:55:43 +0100

    Added some extension methods for FluentValidation

--- ZEA.Validation.FluentValidation/ValidationBehavior.cs
+++ ZEA.Validation.FluentValidation/ValidationBehavior.cs
@@ -8,24 +8,17 @@ namespace ZEA.Validation.FluentValidation;
 /// </summary>
 /// <typeparam name="TRequest"></typeparam>
 /// <typeparam name="TResponse"></typeparam>
-public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>
 {
-	private readonly IEnumerable<IValidator<TRequest>> _validators;
-
-	public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
-	{
-		_validators = validators;
-	}
-
 	public async Task<TResponse> Handle(
 		TRequest request,
 		RequestHandlerDelegate<TResponse> next,
 		CancellationToken cancellationToken)
 	{
-		if (!_validators.Any()) return await next();
+		if (!validators.Any()) return await next();
 
 		var context = new ValidationContext<TRequest>(request);
-		var errorsDictionary = _validators.Select(x => x.Validate(context))
+		var errorsDictionary = validators.Select(x => x.Validate(context))
 			.SelectMany(x => x.Errors)
 			.Where(x => x != null)
 			.ToList();
