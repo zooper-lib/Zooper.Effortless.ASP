7adfa69050df1e8cfa0728c6c95d49588dcdf27e
Daniel <daniel@zooper.dev>
2024-07-01 11:59:22 +0200

    A fix for a constructor

--- ZEA.Communication.Messaging.Abstractions/EventMetadata.cs
+++ ZEA.Communication.Messaging.Abstractions/EventMetadata.cs
@@ -3,15 +3,17 @@ namespace ZEA.Communication.Messaging.Abstractions;
 /// <summary>
 /// The metadata for an event.
 /// </summary>
-public sealed class EventMetadata
+public sealed class EventMetadata(Guid id, DateTime createdAt)
 {
+	public static EventMetadata Create(Guid id, DateTime createdAt) => new(id, createdAt);
+
 	/// <summary>
 	/// Gets the unique identifier for the event.
 	/// </summary>
-	public Guid Id { get; init; } = Guid.NewGuid();
+	public Guid Id { get; init; } = id;
 
 	/// <summary>
 	/// Gets the date and time when the event was created.
 	/// </summary>
-	public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+	public DateTime CreatedAt { get; init; } = createdAt;
 }
