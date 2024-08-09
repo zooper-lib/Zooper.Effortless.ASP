f2cd451b6063cf8d44e9a1cb7e8c144510c3c3bc
Daniel <daniel@zooper.dev>
2024-08-09 11:42:58 +0200

    Small fix
    Made IDomainEvent generic

--- ZEA.Architecture.DDD/Interfaces/IDomainEvent.cs
+++ ZEA.Architecture.DDD/Interfaces/IDomainEvent.cs
@@ -4,3 +4,8 @@ namespace ZEA.Architecture.DDD.Interfaces;
 /// Represents a domain event that is used for indicating state changes within a domain (aka. Service).
 /// </summary>
 public interface IDomainEvent : IEvent;
+
+public interface IDomainEvent<out TMetadata> : IEvent
+{
+	TMetadata Metadata { get; }
+}
