f514c84a9b052da1def5f2b6b770163bc5813a2c
Daniel <daniel@zooper.dev>
2024-10-08 19:43:54 +0200

    Moved the generator attributes to other project

--- ZEA.Communications.Messaging.MassTransit/Attributes/TopicAttribute.cs
+++ ZEA.Communications.Messaging.MassTransit/Attributes/TopicAttribute.cs
@@ -1,6 +1,4 @@
-using System;
-
-namespace ZEA.Communications.Messaging.MassTransit.Generators.Attributes;
+namespace ZEA.Communications.Messaging.MassTransit.Attributes;
 
 [AttributeUsage(AttributeTargets.Class, Inherited = false)]
 public sealed class TopicAttribute(string topicName) : Attribute
