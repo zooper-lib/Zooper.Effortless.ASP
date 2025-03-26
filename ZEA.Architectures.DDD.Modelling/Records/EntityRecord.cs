cd3155a8bcc9c6455382356807fbec9fdbc5d6ef
Daniel <daniel@zooper.dev>
2025-03-26 10:00:30 +0100

    Made the IDs of Entities protected set

--- ZEA.Architectures.DDD.Modelling/Records/EntityRecord.cs
+++ ZEA.Architectures.DDD.Modelling/Records/EntityRecord.cs
@@ -1,3 +1,8 @@
+// ReSharper disable UnusedMember.Global
+
 namespace ZEA.Architectures.DDD.Modelling.Records;
 
-public abstract record EntityRecord<TId>(TId Id) where TId : notnull;
+public abstract record EntityRecord<TId>(TId Id) where TId : notnull
+{
+	public TId Id { get; protected set; } = Id;
+}
