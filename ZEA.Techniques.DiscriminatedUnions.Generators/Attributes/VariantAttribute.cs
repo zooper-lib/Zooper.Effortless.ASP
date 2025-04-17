c709253bbad8b3f28cc1333e47752c31ce0ec910
Daniel <daniel@zooper.dev>
2025-04-17 12:44:44 +0200

    WIP - Removing Generators

--- ZEA.Techniques.DiscriminatedUnions.Generators/Attributes/VariantAttribute.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace ZEA.Techniques.DiscriminatedUnions.Generators.Attributes;
-
-[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
-public sealed class VariantAttribute : Attribute;
